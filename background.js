9ec04bd55483d6be434b395cc7882ba7289e441a
JDevlieghere <info@jonasdevlieghere.com>
2014-07-26 17:33:58 +0200

    Added QST function

--- background.js
+++ background.js
@@ -1,6 +1,32 @@
+var username;
+var api_key;
+load();
+
+function load(){
+    chrome.storage.local.get('username', function (result) {
+    	username = result.username
+    }
+    chrome.storage.local.get('api_key', function (result) {
+    	api_key = result.api_key
+    }
+}
+
 function qst(info){
-	//TODO: Implment
-	return
+	var selection = info.selectionText;
+	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+		function(tabs){
+			var url = tabs[0].url
+			shorten(url, username, api_key, function(url){
+				tweet(selection, url);
+			})
+		}
+	);
+}
+
+function tweet(text, url){
+	var twitter = "https://twitter.com/intent/tweet?source=webclient&text=";
+	var sep = " ";
+	window.open(twitter+text+sep+url,'_blank');
 }
 
 function shorten(long_url, username, api_key, callback){
